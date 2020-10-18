2875e32d895f5313f65786e65c18082fd30016ba
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-10-17 19:57:29 -0500

    Summary of changes
    Added sign-up, sign-in, change-password, and sign-out event listeners
    to the document ready function.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,12 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const events = require('./auth/events')
 
 $(() => {
   // your JS code goes here
+  $('#sign-up-form').on('submit', events.onSignUp)
+  $('#sign-in-form').on('submit', events.onSignIn)
+  $('change-password-form').on('submit', events.onChangePassword)
+  $('sign-out-form').on('submit', events.onSignOut)
 })
