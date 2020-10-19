dc2ed2e54c1592cbeeae446cb10f3132538ec4b2
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-10-19 17:02:20 -0500

    Summary of changes
    added a curl script for the start new game button in html file.

---
+++ curl-scripts/auth/start-game.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+API="https://tic-tac-toe-api-development.herokuapp.com"
+URL_PATH="/games"
+
+curl "${API}${URL_PATH}/" \
+  --include \
+  --request POST \
+  --header "Authorization: Token token=${TOKEN}"
+  --header "Content-type: application/json" \
+  --data '{
+  }'
+
+
+echo
