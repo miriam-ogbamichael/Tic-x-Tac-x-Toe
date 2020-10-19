ad83153c2cd6a44b19ba6574c724b65246d6b3d7
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-10-18 22:43:29 -0500

    Summary of Changes
    Did some console logging to check if I can identify the error message
    of the api.store function being and undefined function but was
    unsuccessful at my attempts.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,31 +1,43 @@
 
-const store = require('../store')
+const store = require('./../store')
 
-const signUpSuccess = function () {
-  $('#message').text('You have sign up successfully!')
+const signUpSuccess = function (response) {
+  $('#message').text('You have sign up successfully ' + response.user.email)
+  console.log('WAHOOOOOO')
 }
 
 const signUpFailure = function () {
   $('#message').text('Oops, there is an error with your sign up.')
 }
 
-const signInSuccess = function () {
-  $('#message').text('You have signed in successfully!')
+const signInSuccess = function (response) {
+  $('#message').text('You have signed in successfully ' + response.user.token)
+  // saves the users information thats in the api resposne to the store.js file
+  store.user = response.user
+  $('#change-password-form').show()
+  $('#sign-out-form').show()
+  $('#sign-up-form').hide()
+  $('#sign-in-form').hide()
 }
 
 const signInFailure = function () {
   $('#message').text('Oops, there is an error with your sign in.')
 }
 
-const signOutSuccess = function () {
+const signOutSuccess = function (response) {
   $('#message').text('You have signed out successfully!')
+  store.user = null
+  $('#change-password-form').hide()
+  $('#sign-out-form').hide()
+  $('#sign-up-form').show()
+  $('#sign-in-form').show()
 }
 
 const signOutFailure = function () {
   $('#message').text('Oops, there is an error with your sign out.')
 }
 
-const changePasswordSuccess = function () {
+const changePasswordSuccess = function (response) {
   $('#message').text('You have changed your password successfully!')
 }
 
