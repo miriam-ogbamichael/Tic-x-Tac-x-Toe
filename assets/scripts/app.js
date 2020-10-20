28e9d9c4b0e87ec66251acdd6d4289e8f111dfed
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-10-20 12:10:22 -0500

    Summary of changes
    Commented out .hide and .show functions to be able to see the entire
    page all at once so that I don't have to complete the entire
    authorization process before I can begin testing out the changes I make
    to the game board.
    
    Also changed the names to be more appropriate for the actions they are
    invoking.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,21 +5,22 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const events = require('./auth/events')
+const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
 
 $(() => {
   // your JS code goes here
-  $('#sign-up-form').on('submit', events.onSignUp)
-  $('#sign-in-form').on('submit', events.onSignIn)
-  $('#change-password-form').on('submit', events.onChangePassword)
-  $('#sign-out-form').on('submit', events.onSignOut)
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
 
-  $('#new-game-form').on('submit', events.onNewGame)
-  $('#game-board-form').on('submit', events.onGameBoard)
+  $('#new-game-form').on('submit', gameEvents.onStartPlaying)
+  $('#game-board-form').on('submit', gameEvents.onGameBoard)
 
-  $('#change-password-form').hide()
-  $('#sign-out-form').hide()
-  $('#sign-in-form').hide()
-  $('#new-game-form').hide()
-  $('#game-board-form').hide()
+  // $('#change-password-form').hide()
+  // $('#sign-out-form').hide()
+  // $('#sign-in-form').hide()
+  // $('#new-game-form').hide()
+  // $('#game-board-form').hide()
 })
