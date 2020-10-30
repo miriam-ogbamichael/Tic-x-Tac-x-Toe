c1b1b91b5b9c534696adce6f82764828ed052576
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-10-29 22:18:02 -0600

    Added objects: board, currentPlayer, & gameOver

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,16 @@
 'use strict'
 
 const store = {
+  // keeping track of all of game board actions
+  board: ['', '', '',
+    '', '', '',
+    '', '', ''],
+  // keeping track of the current player
+  currentPlayer: 'X',
+  // keeping track of whether the game is over or not
+  gameOver: false
+  // count: 0,
+  // truecount: 0
 }
 
 module.exports = store
